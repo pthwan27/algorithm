a0895ceebb28988784d7977ea7a9db9ed6de2d80
pthwan27 <parkssuu@gmail.com>
2022-07-23 01:17:53 +0900

    SWEA 13428

--- 00.algorithm/00.algorithm/src/com/swea/algorithm/Solution.java
+++ 00.algorithm/00.algorithm/src/com/swea/algorithm/Solution.java
@@ -1,79 +1,51 @@
 package com.swea.algorithm;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Arrays;
+import java.util.Scanner;
 
-public class Solution {
-
-	public static void main(String[] args) throws Exception, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int T = Integer.parseInt(br.readLine());
-
-		int c = 0;
-		for (c = 1; c <= T; c++) {
-
-			int N = Integer.parseInt(br.readLine());
-
-			int i = 0;
-			int j = 0;
-			int[][] arr = new int[N][N];
-			int[][] roarr = new int[N][N]; // 90,180,270 순서대로 돌려 저장할 배열
-			String[][] result = new String[N][3];
-
-			for (i = 0; i < N; i++) {
-				st = new StringTokenizer(br.readLine());
-				for (j = 0; j < N; j++) {
-					arr[i][j] = Integer.parseInt(st.nextToken());
-				}
-			}
-
-			String temp = "";
-			int len = N - 1;
-
-			for (int a = 0; a < 3; a++) {
-				roarr = new int[N][N];
-				for (i = 0; i < N; i++) {
-					for (j = 0; j < N; j++) {
-						temp += String.valueOf(arr[len - j][i]);
+class Solution {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
+		
+		int T = sc.nextInt();
+		
+		
+		//한번만 바꾸는 거라면, 자릿수가 제일 중요!		
+		for(int tc = 1; tc <= T; tc++) {
+			int N = sc.nextInt();
+			
+			max = N;
+			min = N;
+			
+			String numStr = String.valueOf(N);			
+			char[] numChar = numStr.toCharArray();	
+			char[] originNumChar = numStr.toCharArray();	
+			for(int i = 0; i < numStr.length(); i++) {
+				for(int j = i + 1; j < numStr.length(); j++){										
+					//숫자 한쌍 위치 바꾸기
+					char c1 = numChar[i];
+					numChar[i] = numChar[j];
+					numChar[j] = c1;
+					
+					int changeNum = Integer.parseInt(String.valueOf(numChar));
+					//첫번째 숫자가 0이면 안되고, 최대값, 최소값 찾기
+					if(numChar[0] != '0' && changeNum > max ) {
+						max = changeNum;
 					}
-
-					result[i][a] = temp;
-					temp = "";
+					
+					if(numChar[0] != '0' && changeNum < min) {
+						min = changeNum;
+					}					
+					
+					//원래대로 돌려놓기
+					numChar = Arrays.copyOf(originNumChar, numChar.length);
 				}
-
-				/*
-				 * 90도 회전 ex 3*3 배열 일 때 0,0 0,1 0,2 1,0 1,1 1,2 2,0 2,1 2,2
-				 * 
-				 * 2,0 1,0 0,0 2,1 1,1 0,1 2,2 1,2 0,2
-				 */
-//		              int [][] arr2 = new int[3][3];
-//		              int temp1 = 2; int temp2 = 2;
-//		              for(i = 0; i < 3; i++) {
-//		                  for(j = 0; j < 3; j++) {
-//		                      arr2[i][j] = arr[temp1-j][i];
-//		                  }
-//		              }
-
-				// 90도 회전시키기
-				for (i = 0; i < N; i++) {
-					for (j = 0; j < N; j++) {
-						roarr[i][j] = arr[len - j][i];
-					}
-				}
-				arr = roarr;
-
-			}
-			System.out.println("#" + c + " ");
-			for (i = 0; i < N; i++) {
-				for (j = 0; j < 3; j++) {
-					System.out.print(result[i][j] + " ");
-				}
-				System.out.println();
-			}
+			}			
+			System.out.printf("#%d %d %d%n", tc, min, max);
 		}
-
 	}
 }
