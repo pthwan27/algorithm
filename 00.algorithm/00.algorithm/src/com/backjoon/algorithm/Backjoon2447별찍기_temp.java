ae79dad14f955fd98e148fdbbd35dd3755813820
pthwan27 <parkssuu@gmail.com>
2022-07-24 00:47:43 +0900

    SWEA6958

--- 00.algorithm/00.algorithm/src/com/backjoon/algorithm/Backjoon2447별찍기_temp.java
+++ 00.algorithm/00.algorithm/src/com/backjoon/algorithm/Backjoon2447별찍기_temp.java
@@ -5,6 +5,17 @@ import java.util.Scanner;
 public class Backjoon2447별찍기_temp {
 	static char[][] arr;
 
+	// 재귀함수를 이용하여 별찍기
+	// 1. 나갈수 있도록
+	// 2. 규칙찾기
+	
+	// N -> 3이면
+	//	***
+	//	* *
+	//	***	 		
+
+	// 2중 for문 , 5번째일 때는 별 찍고, 아닐 땐 *
+
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		int N = in.nextInt();
@@ -24,42 +35,39 @@ public class Backjoon2447별찍기_temp {
 	}
 
 	static void star(int x, int y, int N, boolean blank) {
-	 
-			// 공백칸일 경우
-			if (blank) {
-				for (int i = x; i < x + N; i++) {
-					for (int j = y; j < y + N; j++) {
-						arr[i][j] = ' ';
-					}
+		// 공백칸일 경우
+		if (blank) {
+			for (int i = x; i < x + N; i++) {
+				for (int j = y; j < y + N; j++) {
+					arr[i][j] = ' ';
 				}
-				return;
 			}
-	 
-			// 더이상 쪼갤 수 없는 블록일 때
-			if (N == 1) {
-				arr[x][y] = '*';
-				return;
-			}
-	 
-			/*
-			   N=27 일 경우 한 블록의 사이즈는 9이고, 
-			   N=9 일 경우 한 블록의 사이즈는 3
-			   해당 블록의 한 칸을 담을 변수를 의미 size
-	           
-			   count는 별 출력 누적을 의미
-			 */		
-	 
-			int size = N / 3;
-			int count = 0;
-			for (int i = x; i < x + N; i += size) {
-				for (int j = y; j < y + N; j += size) {
-					count++;
-					if (count == 5) { // 공백 칸일 경우
-						star(i, j, size, true);
-					} else {
-						star(i, j, size, false);
-					}
+			return;
+		}
+
+		// 더이상 쪼갤 수 없는 블록일 때
+		if (N == 1) {
+			arr[x][y] = '*';
+			return;
+		}
+
+		/*
+		 * N=27 일 경우 한 블록의 사이즈는 9이고, N=9 일 경우 한 블록의 사이즈는 3
+		 *  해당 블록의 한 칸을 담을 변수를 의미 size 
+		 * count는 별 출력 누적을 의미
+		 */
+
+		int size = N / 3;
+		int count = 0;
+		for (int i = x; i < x + N; i += size) {
+			for (int j = y; j < y + N; j += size) {
+				count++;
+				if (count == 5) { // 공백 칸일 경우
+					star(i, j, size, true);
+				} else {
+					star(i, j, size, false);
 				}
 			}
 		}
+	}
 }
--- 00.algorithm/00.algorithm/src/com/swea/algorithm/Solution.java
+++ 00.algorithm/00.algorithm/src/com/swea/algorithm/Solution.java
@@ -3,49 +3,31 @@ package com.swea.algorithm;
 import java.util.Arrays;
 import java.util.Scanner;
 
-class Solution {
+public class Solution {
 	public static void main(String[] args) {
-		
 		Scanner sc = new Scanner(System.in);
-		
-		int max = Integer.MIN_VALUE;
-		int min = Integer.MAX_VALUE;
 		
 		int T = sc.nextInt();
 		
+		int [] kyuCard = new int[9]; //규영이		
+		int [] InCard = new int[9]; //인영이
 		
-		//한번만 바꾸는 거라면, 자릿수가 제일 중요!		
-		for(int tc = 1; tc <= T; tc++) {
-			int N = sc.nextInt();
+		boolean [] CardCheck = new boolean[18]; 
+		
+		for(int tc= 0; tc < T; tc++) {
+			for(int i = 0; i < 9; i++) {
+				kyuCard[i] = sc.nextInt();
+				CardCheck[kyuCard[i]] = true;
+			}
 			
-			max = N;
-			min = N;
-			
-			String numStr = String.valueOf(N);			
-			char[] numChar = numStr.toCharArray();	
-			char[] originNumChar = numStr.toCharArray();	
-			for(int i = 0; i < numStr.length(); i++) {
-				for(int j = i + 1; j < numStr.length(); j++){										
-					//숫자 한쌍 위치 바꾸기
-					char c1 = numChar[i];
-					numChar[i] = numChar[j];
-					numChar[j] = c1;
-					
-					int changeNum = Integer.parseInt(String.valueOf(numChar));
-					//첫번째 숫자가 0이면 안되고, 최대값, 최소값 찾기
-					if(numChar[0] != '0' && changeNum > max ) {
-						max = changeNum;
-					}
-					
-					if(numChar[0] != '0' && changeNum < min) {
-						min = changeNum;
-					}					
-					
-					//원래대로 돌려놓기
-					numChar = Arrays.copyOf(originNumChar, numChar.length);
+			int count = 0;
+			for(int i = 0; i < 17; i++) {				
+				if(CardCheck[i] == false) {
+					InCard[count++] = i;
 				}
 			}			
-			System.out.printf("#%d %d %d%n", tc, min, max);
 		}
+		
+		
 	}
 }
