70850589eaec3e533c5e029b7a76cc888f1f0f8f
sbsc20 <sbsc20@naver.com>
2022-08-12 17:45:09 +0900

    algo 0812

--- 00.algorithm/00.algorithm/src/com/backjoon/algorithm/BJ_14888_연산자끼워넣기.java
+++ 00.algorithm/00.algorithm/src/com/backjoon/algorithm/BJ_14888_연산자끼워넣기.java
@@ -4,18 +4,36 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 /**
- * 연산자끼워넣기, 숫자의 갯수를 알려주고, 숫자를 입력받는다. * 
- * 연산자들을 받는데, 숫자의 갯수 -1개 만큼 덧셈, 뺄셈, 곱셈, 나눗셈 순서대로 받는다. * 
+ * 연산자끼워넣기, 숫자의 갯수를 알려주고, 숫자를 입력받는다. * 연산자들을 받는데, 숫자의 갯수 -1개 만큼 덧셈, 뺄셈, 곱셈, 나눗셈
+ * 순서대로 받는다. *
+ * 
  * @author parks
  */
 public class BJ_14888_연산자끼워넣기 {
+
+	static int N;
+	
+	// 숫자가 담겨있는 배열
+	static int [] numArr;
+	
+	// 연산자가 담겨있는 배열
+	static char[] operCharArr;
+
+	// 순열 만들기 위한 checked 배열
+	static boolean[] isChecked;
+
+	// comChar -> 연산자 순열 담는 배열
+	static char[] comChar;
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int N = Integer.parseInt(br.readLine());
+		N = Integer.parseInt(br.readLine());
 
 		// 숫자 갯수만큼 배열 생성
 		int[] numArr = new int[N];
@@ -29,18 +47,20 @@ public class BJ_14888_연산자끼워넣기 {
 		// 연산자의 갯수 카운팅( 덧셈, 뺄셈, 곱셈, 나눗셈 )
 		int[] operCountArr = new int[4];
 		st = new StringTokenizer(br.readLine(), " ");
-		
-		//count 연산자 있는 것 만큼 카운팅하기 위해 생성
-		int operCnt = 0; 
-		for (int idx = 0; idx < 4; idx++) {			
-			operCnt += operCountArr[idx] = Integer.parseInt(st.nextToken());			
-		}		
-		
-		char[] operCharArr = new char[operCnt];
-		
+
+		// count 연산자 있는 것 만큼 카운팅하기 위해 생성
+		int operCnt = 0;
+		for (int idx = 0; idx < 4; idx++) {
+			operCnt += operCountArr[idx] = Integer.parseInt(st.nextToken());
+		}
+
+		// 연산자를 담을 배열을 초기화
+		operCharArr = new char[operCnt];
+	
+		// >> 숫자로 그냥 받아두고 , 계산할 때 처리해도 됌 <<
 		int count = 0;
-		for(int idx = 0; idx < 4; idx++) {
-			while(operCountArr[idx]-- > 0) {
+		for (int idx = 0; idx < 4; idx++) {
+			while (operCountArr[idx]-- > 0) {
 				switch (idx) {
 				case 0:
 					operCharArr[count++] = '+';
@@ -59,9 +79,29 @@ public class BJ_14888_연산자끼워넣기 {
 				}
 			}
 		}
-		
-		//연산자의 순열을 만들고, 이를 활용해 계산해보기
-		//숫자위치 고정 ( 연산자를 돌려가며 계산 해보기 )
+		// 연산자의 순열을 만들고, 이를 활용해 계산해보기
+		// 숫자위치 고정 ( 연산자를 돌려가며 계산 해보기 )
+		isChecked = new boolean[N - 1];
+		comChar = new char[N - 1];
+		comb(0);
+	}
+
+	private static void comb(int cur) {
+		if (cur == N - 1) {
+			calculator();
+		}
+		for (int i = 0; i < N - 1; i++) {
+			if (isChecked[i] == true)
+				continue;
+
+			isChecked[i] = true;
+			comChar[cur] = operCharArr[i];
+			comb(cur + 1);
+			isChecked[i] = false;
+		}
+	}
+
+	private static void calculator() {		
 		
 	}
 }
---
+++ 00.algorithm/00.algorithm/src/com/backjoon/algorithm/BJ_15686_치킨배달_박태환.java
@@ -0,0 +1,99 @@
+package com.backjoon.algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class BJ_15686_치킨배달_박태환 {
+
+	/**
+	 * 크기가 N * N N*N 은 빈 칸, 치킨집, 집 중 하나 이다. r행 c열, r/c는 1부터 시작한다. 치킨거리 - 집과 가장 가까운
+	 * 치킨집 사이의 거리 이고 도시의 치킨거리는 모든 집의 치킨 거리의 합 이다.
+	 * 
+	 * 이 때 도시에 M개 만큼의 치킨집만 있어야 할 때, 어떤 M개를 골라야 도시의 치킨거리가 가장 낮게 나오는지 확인하고, 그 가장 낮은
+	 * 도시의 치킨거리를 출력.
+	 * 
+	 */
+	static int N; // 배열 크기
+	static int M; // 있어야하는 치킨집 갯수
+
+	static int[][] map; // 맵 정보 저장
+
+	static ArrayList<int[]> chickenMap; // 치킨집의 위치 저장
+
+	static int[] SelectNum; // 조합 저장 (중복 x)
+
+	static int result = Integer.MAX_VALUE;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		map = new int[N + 1][N + 1];
+		chickenMap = new ArrayList<>();
+
+		for (int r = 1; r <= N; r++) {
+			st = new StringTokenizer(in.readLine());
+			for (int c = 1; c <= N; c++) {
+				int inputNum = Integer.parseInt(st.nextToken());
+
+				if (inputNum == 2) {
+					chickenMap.add(new int[] { r, c });
+				}
+				map[r][c] = inputNum;
+			}
+		}
+//		print(chickenMap); // 입력확인 완료
+
+		// 조합 뽑기 (치킨 집을 M개 만큼 뽑을 수 있는 경우의 수)
+		SelectNum = new int[M];
+		comb(0, 0); // 현재위치, 출발점
+		System.out.println(result);
+	}
+
+	private static void comb(int cur, int start) {
+		if (cur == M) {
+//			System.out.println(Arrays.toString(SelectNum));
+			// 구해 둔 조합을 돌면서, 최소거리 찾기
+			result = Math.min(result, calcLen(SelectNum));
+			return;
+		}
+
+		for (int idx = start; idx < chickenMap.size(); idx++) {
+			SelectNum[cur] = idx;
+			comb(cur + 1, idx + 1);
+		}
+	}
+
+	private static int calcLen(int[] selectNum) {
+		int minDistance = Integer.MAX_VALUE;
+		int Sum = 0;
+
+		for (int r = 1; r < map.length; r++) {
+			for (int c = 1; c < map[r].length; c++) {
+				// 선택해둔 치킨집 2개와의 거리 비교하고, 작은걸로 더하기
+				if (map[r][c] == 1) {
+					for (int i = 0; i < M; i++) {
+						// 치킨집과의 거리 비교하면서 가장 짧은 거리 MinSum에 저장
+						// System.out.println(chickenMap.get(selectNum[0])[0] + " " +
+						// chickenMap.get(selectNum[0])[1]);
+						int chickenR = chickenMap.get(selectNum[i])[0];
+						int chickenC = chickenMap.get(selectNum[i])[1];
+
+						int distance = Math.abs(chickenR - r) + Math.abs(chickenC - c);
+						minDistance = Math.min(minDistance, distance);
+					}
+					Sum += minDistance;
+					minDistance = Integer.MAX_VALUE;
+				}
+			}
+		}
+		return Sum;
+	}
+
+}
