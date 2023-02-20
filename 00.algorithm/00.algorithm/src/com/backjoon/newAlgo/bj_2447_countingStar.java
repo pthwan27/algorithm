6ea1cca47c53902d768945f1a753e4633ee4c77b
pthwan27 <parkssuu@gmail.com>
2023-02-20 22:04:39 +0900

    "algo day0220"

--- 00.algorithm/00.algorithm/src/com/backjoon/newAlgo/bj_2447_countingStar.java
+++ 00.algorithm/00.algorithm/src/com/backjoon/newAlgo/bj_2447_countingStar.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class bj_2447_countingStar {
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(in.readLine());
