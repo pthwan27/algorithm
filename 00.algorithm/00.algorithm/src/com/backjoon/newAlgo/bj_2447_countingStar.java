6d91523faf1b5472fd69ea28599f7de895cc1aca
pthwan27 <parkssuu@gmail.com>
2023-02-28 01:39:59 +0900

    "FIX : Remove unused imports"

--- 00.algorithm/00.algorithm/src/com/backjoon/newAlgo/bj_2447_countingStar.java
+++ 00.algorithm/00.algorithm/src/com/backjoon/newAlgo/bj_2447_countingStar.java
@@ -3,8 +3,6 @@ package com.backjoon.newAlgo;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
 
 public class bj_2447_countingStar {
     static StringBuilder sb = new StringBuilder();
