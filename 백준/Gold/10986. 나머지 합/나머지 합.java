a2cbedec2a79e1a7293598f9dd4c62e0efa1e10e
pthwan27 <65005680+pthwan27@users.noreply.github.com>
2023-06-14 11:25:18 +0900

    [Gold III] Title: 나머지 합, Time: 504 ms, Memory: 120692 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.java
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -16,8 +16,7 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
-		sumArr = new long[N];
-		
+		sumArr = new long[N];		
 		//나머지의 갯수
 		remainArr = new long[M];
 		
@@ -26,12 +25,13 @@ public class Main {
 		
 		st = new StringTokenizer(in.readLine());
 		
+		//누적합구하기
 		sumArr[0] = Integer.parseInt(st.nextToken());
-		
 		for(int i =1; i < N; i++) {
 			sumArr[i] = sumArr[i-1] + Integer.parseInt(st.nextToken());
 		}
 		
+		//나머지 갯수 찾기, 이 때 누적합이 0이다? -> 구간의 합이 M으로 나누어 떨어진다 -> result++
 		for(int i = 0; i < N; i++) {
 			int remainder = (int) (sumArr[i] % M);
 			if(remainder == 0) {
@@ -39,8 +39,10 @@ public class Main {
 			}
 			remainArr[remainder]++;
 		}
-		
 
+		// 나머지개수 C２
+		// 나머지 개수가 2개 이상인것들은 크기가 2인 조합을 통해 갯수를 찾는다 
+		// -> 1~3 , 1~5의 나머지가 같으면 1~5 빼기 1~3을 빼면 나머지가 0, 0은? M으로 나누어 떨어진다. -> result++
 		for(int i = 0; i < M; i++) {
 			if(remainArr[i] > 1) {
 				result += (remainArr[i] * (remainArr[i] - 1) / 2 );
