1bfc30a2a0ea2773d2e27ef99083e039e4682747
pthwan27 <65005680+pthwan27@users.noreply.github.com>
2023-07-17 10:57:20 +0900

    [Silver I] Title: 회의실 배정, Time: 660 ms, Memory: 51436 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -1,69 +1,50 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-/**
- * 회의실 배정. Comparator를 써서 ? X 정렬해놓고, 재귀로?
- * 
- * @author
- *
- */
 public class Main {
-	static int N;;
-	static ArrayList<int[]> timesList;
-
-	static int cnt = 1;
-	static int result = 0;
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-
-		N = Integer.parseInt(in.readLine());
-
-		timesList = new ArrayList<>();
-		for (int idx = 0; idx < N; idx++) {
-			StringTokenizer st = new StringTokenizer(in.readLine());
-			int startTime = Integer.parseInt(st.nextToken());
-			int endTime = Integer.parseInt(st.nextToken());
-
-			int[] times = new int[] { startTime, endTime };
-
-			timesList.add(idx, times);
+	static class room implements Comparable<room>{
+		int startTime, endTime;
+		
+		public room(int start, int end) {
+			this.startTime = start;
+			this.endTime = end;
 		}
-		sortList(timesList);
 		
-		int endTime = timesList.get(0)[1];
 		
-		for (int idx = 1; idx < timesList.size(); idx++) {		
-			if(endTime > timesList.get(idx)[0]) {
-				continue;
+		@Override
+		public int compareTo(room o) {
+			if(this.endTime == o.endTime) {
+				return this.startTime - o.startTime;
+			}else {				
+				return this.endTime - o.endTime;
 			}
-			else {
-				endTime = timesList.get(idx)[1];
-				cnt++;
-			}			
 		}
-		System.out.println(cnt);
 	}
-	
-	//종료시간 오름차순으로 정렬, 만약 종료시간이 같으면 시작시간이 낮은것부터 나오도록 정렬
-	public static void sortList(ArrayList<int[]> timesList) {
-		Collections.sort(timesList, new Comparator<int[]>() {
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				if (o1[1] == o2[1]) {
-					return Integer.valueOf(o1[0]).compareTo(Integer.valueOf((o2[0])));
-				} else {
-					return Integer.valueOf(o1[1]).compareTo(Integer.valueOf((o2[1])));
-				}
-
+	public static void main(String[] args) throws IOException{
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(in.readLine());
+		ArrayList<room> roomList = new ArrayList<>();
+		
+		for(int i = 0; i < N; i++) {
+			String[] times = in.readLine().split(" ");
+			roomList.add(new room(Integer.parseInt(times[0]),Integer.parseInt(times[1])));
+		}
+		Collections.sort(roomList);
+		
+		int endTime = roomList.get(0).endTime;
+		int cnt = 1;
+		for(int i = 1; i < N; i++) {		
+			if(endTime > roomList.get(i).startTime) {
+				continue;
+			}else {
+				endTime = roomList.get(i).endTime;
+				cnt++;
 			}
-		});
+			
+		}
+		
+		System.out.println(cnt);
 	}
 
 }
