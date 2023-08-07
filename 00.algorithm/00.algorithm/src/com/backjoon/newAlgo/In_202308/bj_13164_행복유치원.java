3ed89c1702af87926308a0e958360ffe67b10578
pthwan27 <parkssuu@gmail.com>
2023-08-07 13:27:26 +0900

    feat: add 0807

---
+++ 00.algorithm/00.algorithm/src/com/backjoon/newAlgo/In_202308/bj_13164_행복유치원.java
@@ -0,0 +1,37 @@
+package com.backjoon.newAlgo.In_202308;
+
+import java.io.*;
+import java.util.*;
+
+public class bj_13164_행복유치원 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+		String[] inputs = in.readLine().split(" ");
+
+		int N = Integer.parseInt(inputs[0]);
+		int K = Integer.parseInt(inputs[1]);
+
+		int[] studentArr = new int[N];
+		int[] diffArr = new int[N - 1];
+
+		inputs = in.readLine().split(" ");
+		for (int i = 0; i < N; i++) {
+			studentArr[i] = Integer.parseInt(inputs[i]);
+			if (i > 0) {
+				diffArr[i - 1] = studentArr[i] - studentArr[i - 1];
+			}
+		}
+		
+		Arrays.sort(diffArr);
+
+		int result = 0;
+		for (int i = 0; i < (N - 1) - (K - 1); i++) {
+			result += diffArr[i];
+		}
+
+		System.out.println(result);
+	}
+
+}
---
+++ 00.algorithm/00.algorithm/src/com/backjoon/newAlgo/In_202308/bj_1793_타일링.java
@@ -0,0 +1,19 @@
+package com.backjoon.newAlgo.In_202308;
+
+import java.io.*;
+import java.util.*;
+
+public class bj_1793_타일링 {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(in.readLine());
+		
+		
+		
+		
+		
+	}
+
+}
