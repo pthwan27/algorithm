82a714e6de735cc15816352930221d09a08d09b0
pthwan27 <65005680+pthwan27@users.noreply.github.com>
2023-08-08 11:56:24 +0900

    [Gold V] Title: 수 고르기, Time: 368 ms, Memory: 28184 KB -BaekjoonHub

---
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -0,0 +1,42 @@
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	static int N, M;
+	
+	static int minDiff = Integer.MAX_VALUE;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+		String[] inputs = in.readLine().split(" ");
+
+		N = Integer.parseInt(inputs[0]);
+		M = Integer.parseInt(inputs[1]);
+		
+		int[] arr = new int[N];
+		
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(in.readLine());
+		}
+		
+		Arrays.sort(arr);
+		
+		int start = 0, end = 0;
+		
+		while(start <= end && end < N) {
+			int diff = arr[end] - arr[start];
+			
+			if(diff >= M) {
+				minDiff = Math.min(diff , minDiff);
+				start++;
+			}else {
+				end++;				
+			}
+		}
+		
+		System.out.println(minDiff);
+	}
+}
