bcd476840435b22bb6a4992d9d8d69cef895dc3c
pthwan27 <65005680+pthwan27@users.noreply.github.com>
2023-09-06 09:30:13 +0900

    [Silver I] Title: Z, Time: 124 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.java
+++ 백준/Silver/1074. Z/Z.java
@@ -1,58 +1,46 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 
-	static int N, R, C;
+	static int N, r, c;
 
 	static int result = 0;
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(in.readLine());
-		N = Integer.parseInt(st.nextToken());
-		R = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
 
-		int Size = (int) Math.pow(2, N);		
-		find(R, C, Size);
-		
+		String[] inputs = in.readLine().split(" ");
+
+		N = Integer.parseInt(inputs[0]);
+		r = Integer.parseInt(inputs[1]);
+		c = Integer.parseInt(inputs[2]);
+
+		int size = (int) Math.pow(2, N);
+
+		find(r, c, size);
+
 		System.out.println(result);
 	}
 
-	private static void find(int r, int c, int size) {	
-		//목표 위치에 도착
-		if(size == 1) {
+	static void find(int r, int c, int size) {
+		if (size == 1) {
 			return;
 		}
-		
-		// result += Math.pow(divSize,2) * 1;
-		// 재귀로 나눠지는 사각형의 시작 순번(0,0) 더해주기		
-		
+
 		int divSize = size / 2;
-		
-		//4분의 1씩 나눠서 탐색
-		//왼쪽 위 탐색
-		if(r < divSize && c < divSize) {
-			find(r, c, divSize);			
-		}		
-		//오른쪽 위
-		else if(r < divSize && c >= divSize) {			
-			result += Math.pow(divSize,2) * 1;
-			find(r, c-divSize, divSize);
-			
+
+		if (r < divSize && c < divSize) {
+			find(r, c, divSize);
+		} else if (r < divSize && c >= divSize) {
+			result += Math.pow(divSize, 2) * 1;
+			find(r, c - divSize, divSize);
+		} else if (r >= divSize && c < divSize) {
+			result += Math.pow(divSize, 2) * 2;
+			find(r - divSize, c, divSize);
+		} else if (r >= divSize && c >= divSize) {
+			result += Math.pow(divSize, 2) * 3;
+			find(r - divSize, c - divSize, divSize);
 		}
-		//왼쪽 아래
-		else if(r >= divSize && c < divSize) {
-			result += Math.pow(divSize,2) * 2;
-			find(r-divSize, c, divSize);
-		}
-		//오른쪽 아래
-		else {
-			result += Math.pow(divSize,2) * 3;
-			find(r-divSize, c-divSize, divSize);
-		}		
 	}
 }
