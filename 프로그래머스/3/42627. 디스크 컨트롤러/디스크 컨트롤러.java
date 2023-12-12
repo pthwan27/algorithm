8e3e74315e0ed219bce6f387d6f3cc17c9b7a47a
pthwan27 <65005680+pthwan27@users.noreply.github.com>
2023-12-12 15:17:09 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 1.78 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -3,31 +3,28 @@ import java.util.*;
 class Solution {
     public int solution(int[][] jobs) {
         int answer = 0;
-        int endTime = 0; // 작업 끝난 시간
-        int count = 0; // 작업이 처리된 갯수
+        int endTime = 0;
+        int count = 0;
         
-        int jobIndex = 0;
+        Arrays.sort(jobs, (o1,o2) -> o1[0] - o2[0]);
         
-        Arrays.sort(jobs, (o1, o2) -> o1[0] - o2[0]);
         PriorityQueue<int[]> pQ = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
-    
-        int jobsLen = jobs.length;
-        while(count < jobsLen){
-            //endTime보다 같거나, 일찍 시작한 요청들 넣기
-            while(jobIndex < jobsLen && endTime >= jobs[jobIndex][0]){
-                pQ.offer(jobs[jobIndex++]);
+        
+        int idx = 0;
+        while(count < jobs.length){
+            while(idx < jobs.length && endTime >= jobs[idx][0]){
+                pQ.offer(jobs[idx++]);
             }
             
             if(pQ.isEmpty()){
-                endTime = jobs[jobIndex][0];
+                endTime = jobs[idx][0];
             }else{
-                int[] pollJob = pQ.poll();
-                
-                answer += endTime - pollJob[0] + pollJob[1];
+                int[] pollTask = pQ.poll();
+                answer += endTime - pollTask[0] + pollTask[1];
                 count++;
-                endTime += pollJob[1];
+                endTime += pollTask[1];
             }
         }
-        return answer / jobsLen;
+        return answer / jobs.length;
     }
 }
