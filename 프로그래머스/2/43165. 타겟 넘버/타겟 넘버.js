72060da0eaa9012e9e6a52331a316a0ed8a26d99
pthwan27 <parkssuu@gmail.com>
2024-03-28 14:32:56 +0900

    update : 타겟 넘버 수정

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -5,7 +5,7 @@ function solution(numbers, target) {
         if (idx === numbers.length) {
             return sum === target ? 1 : 0;
         }
-
+        
         const key = `${idx},${sum}`;
         if (memo.has(key)) {
             return memo.get(key);
@@ -14,7 +14,14 @@ function solution(numbers, target) {
         const add = dfs(idx + 1, sum + numbers[idx]);
         const subtract = dfs(idx + 1, sum - numbers[idx]);
 
-        memo.set(key, add + subtract);
+        // 각각의 경우에 대해 결과를 메모이제이션
+        memo.set(`${idx + 1},${sum + numbers[idx]}`, add);
+        memo.set(`${idx + 1},${sum - numbers[idx]}`, subtract);
+
+        // 현재 상태의 결과를 반환합니다. 
+        // => (4,4)에서 (5,3) (5,5) 2가지의 경우가 있는데
+        // 여기서 타겟넘버는 1을 반환, 아니면 0를 반환하는데 
+        // 이 2개를 더해서 윗 단계 (4,4)에 넘겨준다
         return add + subtract;
     }
 
