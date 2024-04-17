e5d8717ed8c21a5e256fc59181721574c87b6920
pthwan27 <parkssuu@gmail.com>
2024-04-17 18:29:35 +0900

    update : programmers 요격시스템

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.js
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.js
@@ -1,20 +1,20 @@
 function solution(targets) {
-    targets.sort(([s1,e1], [s2,e2]) => {
-        if(e1 === e2) return s2 - s1;
-        else return e1 - e2;
-    });
-    console.log(targets);
-    let answer = 1;
-    let e = targets[0][1];
-    for(let i = 1; i < targets.length; i++){
-        const nextTarget = targets[i];
-        
-        if(e > nextTarget[0] && e <= nextTarget[1]) continue;
-        else {
-            answer++;
-            e = nextTarget[1];
-        }
+  targets.sort(([s1, e1], [s2, e2]) => {
+    if (e1 === e2) return s2 - s1;
+    else return e1 - e2;
+  });
+  let answer = 1;
+  let e = targets[0][1];
+
+  for (let i = 1; i < targets.length; i++) {
+    const nextTarget = targets[i];
+
+    if (e > nextTarget[0] && e <= nextTarget[1]) continue;
+    else {
+      answer++;
+      e = nextTarget[1];
     }
-    
-    return answer;
+  }
+
+  return answer;
 }
--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.js
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.js
@@ -1,65 +1,66 @@
 class node {
-  constructor() {
-    this.i = -1;
-    this.o = -1;
-  }
-  in() {
-    if (this.i == -1) {
-      this.i = 1;
-      this.o = 0;
-    } else this.i++;
-  }
-  out() {
-    if (this.o == -1) {
-      this.o = 1;
-      this.i = 0;
-    } else this.o++;
-  }
-}
-function solution(edges) {
-  let maxIdx = edges.reduce((acc, edge) => Math.max(acc, edge[0], edge[1]), -1);
-  let inout = new Array(maxIdx + 1).fill().map(() => new node());
-  let generatedV = -1;
-  let generatedVout = -1;
-
-  edges.forEach(([a, b]) => {
-    inout[a].out();
-    inout[b].in();
-  });
-
-  for (let i = 1; i <= maxIdx; i++) {
-    if (inout[i].i === 0 && inout[i].o >= 2) {
-      generatedV = i;
-      generatedVout = inout[i].o;
-      break;
+    constructor() {
+      this.i = -1;
+      this.o = -1;
+    }
+    in() {
+      if (this.i == -1) {
+        this.i = 1;
+        this.o = 0;
+      } else this.i++;
+    }
+    out() {
+      if (this.o == -1) {
+        this.o = 1;
+        this.i = 0;
+      } else this.o++;
     }
   }
-
-  edges.forEach(([a, b]) => {
-    if (edge[0] == generatedV) {
-      inout[a].o--;
-      inout[b].i--;
+  function solution(edges) {
+    let maxIdx = edges.reduce((acc, edge) => Math.max(acc, edge[0], edge[1]), -1);
+    let inout = new Array(maxIdx + 1).fill().map(() => new node());
+    let generatedV = -1;
+    let generatedVout = -1;
+  
+    edges.forEach(([a, b]) => {
+      inout[a].out();
+      inout[b].in();
+    });
+  
+    for (let i = 1; i <= maxIdx; i++) {
+      if (inout[i].i === 0 && inout[i].o >= 2) {
+        generatedV = i;
+        generatedVout = inout[i].o;
+        break;
+      }
     }
-  });
-
-  let stickS = 0;
-  let stickE = 0;
-  let eight = 0;
-
-  for (let i = 1; i <= maxIdx; i++) {
-    if (i == generatedV) continue;
-    if (inout[i].i == 2 && inout[i].o == 2) eight++;
-    if (inout[i].i == 1 && inout[i].o == 0) stickE++;
-    if (inout[i].i == 0 && inout[i].o == 1) stickS++;
-    if (inout[i].i == 0 && inout[i].o == 0) {
-      stickS++;
-      stickE++;
+  
+    edges.forEach(([a, b]) => {
+      if (a == generatedV) {
+        inout[a].o--;
+        inout[b].i--;
+      }
+    });
+  
+    let stickS = 0;
+    let stickE = 0;
+    let eight = 0;
+  
+    for (let i = 1; i <= maxIdx; i++) {
+      if (i == generatedV) continue;
+      if (inout[i].i == 2 && inout[i].o == 2) eight++;
+      if (inout[i].i == 1 && inout[i].o == 0) stickE++;
+      if (inout[i].i == 0 && inout[i].o == 1) stickS++;
+      if (inout[i].i == 0 && inout[i].o == 0) {
+        stickS++;
+        stickE++;
+      }
     }
+    return [
+      generatedV,
+      generatedVout - (Math.min(stickE, stickS) + eight),
+      Math.min(stickE, stickS),
+      eight,
+    ];
   }
-  return [
-    generatedV,
-    generatedVout - (Math.min(stickE, stickS) + eight),
-    Math.min(stickE, stickS),
-    eight,
-  ];
-}
+  
