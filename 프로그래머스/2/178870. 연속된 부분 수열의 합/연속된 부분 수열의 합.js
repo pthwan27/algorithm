6748163acf38094e894d485143844827023ec4eb
pthwan27 <parkssuu@gmail.com>
2024-04-19 15:48:36 +0900

    update : programmers 연속된 부분 수열의 합

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.js
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.js
@@ -1,27 +1,24 @@
 function solution(sequence, k) {
-    let answer = [];
-    
-    let s = 0;
-    let e = 0;
-    let sum = sequence[0];
-    let sumMinLen = sequence.length;
-    
-    while(s < sequence.length && e < sequence.length && s <= e){
-        if(sum == k && sumMinLen > e-s){
-            answer = [s,e];
-            sumMinLen = e-s;
-        }
-        
-        if(sum <= k) {
-            e++;
-            sum += sequence[e];
-        }        
-        
-        if(sum > k){
-            sum -= sequence[s];
-            s++;
-        }        
-        // console.log(`${sum} / ${s}, ${e} // ${answer}` );
+  let answer = [];
+
+  let s = 0;
+  let e = 0;
+  let sum = sequence[0];
+  let shortestLength = sequence.length;
+
+  while (s < sequence.length && e < sequence.length && s <= e) {
+    if (sum === k && shortestLength > e - s) {
+      answer = [s, e];
+      shortestLength = e - s;
     }
-    return answer;
+
+    if (sum <= k) {
+      e++;
+      sum += sequence[e];
+    } else {
+      sum -= sequence[s];
+      s++;
+    }
+  }
+  return answer;
 }
