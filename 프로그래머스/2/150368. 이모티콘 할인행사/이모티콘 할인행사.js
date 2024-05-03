f2e1ebf0600cdbdc91a101ad461890fbc8c49566
pthwan27 <65005680+pthwan27@users.noreply.github.com>
2024-05-03 10:54:43 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 0.52 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.js
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.js
@@ -1,50 +1,58 @@
 function solution(users, emoticons) {
-    let answer = new Array(2).fill(0);
-    
+    let answer = [];
     let discounts = [10, 20, 30, 40];
     
     const comb = (arr, size) => {
-        if(size === 1) return arr.map((e) => [e]);
+        if(size === 1) return arr.map(e => [e]);
         
-        let resultArr = [];
+        let result = [];
         
-        arr.forEach((e, i, origin) => {
-            let fixer = e
+        arr.forEach((e, idx, origin) => {
+            let fixed = e;
             let combArr = comb(origin, size - 1);
-            let attachArr = combArr.map((v) => [fixer, ...v]);
-
-            resultArr.push(...attachArr);              
+            let attachedArr = combArr.map(v => [fixed, ...v]);
+            
+            result.push(...attachedArr);
         });
-        return resultArr;
+        return result;
     }
-    
-    let emoDiscounts = comb(discounts, emoticons.length);
-    
-    emoDiscounts.forEach((discount, idx) => {        
-        let subsCnt = 0;
-        let totalAmt = 0;
-        
-        users.forEach((user) => {
-            let amt = 0;
-            
-            emoticons.forEach((emo, idx) => {
-                if(user[0] <= discount[idx]){
-                    amt += (emoticons[idx] * (100 - discount[idx])) / 100;
-                }
-            });
-            
-            if(amt >= user[1]){
-                subsCnt++;
-                amt = 0;
-            }else{
-                totalAmt += amt;
+    const calc = (u, d, e) => {
+        let result = new Array(2).fill(0);
+        e.forEach((emoticon, i) => {
+            if(u[0] <= d[i]) {
+                result[0] += (emoticon * (100 - d[i])) / 100;
             }
         });
+        if(result[0] >= u[1]){
+            result[0] = 0;
+            result[1] = 1;
+        }
+        return result;
+    }
+    
+    let discountArr = comb(discounts, emoticons.length);
+    
+    let maxSubscriber = -1;
+    let maxTotal = -1;
+    
+    discountArr.forEach((discount, idx) => {
+        let totalSubscriber = 0;
+        let totalPrice = 0;
         
-        if(subsCnt > answer[0] || (subsCnt === answer[0] && totalAmt > answer[1])){
-            answer = [subsCnt, totalAmt];
+        users.forEach(user => {
+            let [price, subscriber] = calc(user, discount, emoticons);
+            totalPrice += price;
+            totalSubscriber += subscriber;
+        });
+        
+        if(totalSubscriber > maxSubscriber){
+            maxSubscriber = totalSubscriber;
+            maxTotal = totalPrice;
+        }else if(totalSubscriber === maxSubscriber && totalPrice > maxTotal){
+            maxTotal = totalPrice;
         }
     });
     
-    return answer;
+    
+    return [maxSubscriber, maxTotal];
 }
