4ae0fcebc904d637de26afec50e22575ea78400f
pthwan27 <65005680+pthwan27@users.noreply.github.com>
2024-05-16 15:33:21 +0900

    [level 3] Title: 표 병합, Time: 20.05 ms, Memory: 39.9 MB -BaekjoonHub

--- 프로그래머스/3/150366. 표 병합/표 병합.js
+++ 프로그래머스/3/150366. 표 병합/표 병합.js
@@ -1,101 +1,118 @@
-
-
-const mergeValue = new Map();
 function solution(commands) {
     let answer = [];
-    let map = Array.from({length: 51}, () => new Array(51));
     
-    for(let r = 1; r <= 50; r++){
-        for(let c = 1; c <= 50; c++){
-            map[r][c] = [0, 'EMPTY'];
-        }    
-    }
+    const valueMap = Array.from(new Array(51), () => new Array(51).fill('EMPTY'));
+    const groupNumMap = Array.from(new Array(51), () => new Array(51).fill(0));
     
-    let groupNum = 1;
+    const group = new Map();
+    
+    let gn = 1;
+    
     commands.forEach(e => {
-        let [command, ...rest] = e.split(' ');
-        if(command === 'UPDATE'){
-            if(rest.length === 3){
-                updateOne(rest, map);
+        let [command, ...rest] = e.split(" ");
+        switch (command) {
+            case 'UPDATE' : {
+                if(rest.length === 3) {
+                    let [r,c,v] = rest;
+                    updateOne(r,c,v);
+                }else{
+                    let [v1, v2] = rest;
+                    updateTwo(v1, v2);
+                }
+            } break;
+            case 'MERGE' : {
+                let [r1, c1, r2, c2] = rest;
+                if(merge(r1, c1, r2, c2, gn)) gn++;
+            } break;
+            case 'UNMERGE' : {
+                let [r,c] = rest;
+                unmerge(r, c);
+            } break;
+            case 'PRINT' :{
+                let [r,c] = rest;
+                print(r,c);
+            } break;
                 
-            }else{
-                updateAll(rest, map);
-            }
-        }
-        else if(command === 'MERGE'){
-            groupNum = merge(rest, map, groupNum) ? groupNum + 1 : groupNum;            
-        }
-        else if(command === 'UNMERGE'){
-            unmerge(rest, map);
-        }
-        else {
-            const [r,c] = rest;
-            answer.push(map[r][c][1]);
+            default : break;
         }
     });
     
+    
+    function updateOne(r, c, v){
+        let groupNum = groupNumMap[r][c];
+        if(groupNum === 0) valueMap[r][c] = v;        
+        else{
+            group.set(groupNum, v);
+            for(let ri = 1; ri < 51; ri++){
+                for(let cj = 1; cj < 51; cj++){
+                    if(groupNumMap[ri][cj] === groupNum) valueMap[ri][cj] = v;
+                }    
+            } 
+        }
+    }
+    function updateTwo(v1, v2){
+        for(let r = 1; r < 51; r++){
+            for(let c = 1; c < 51; c++){
+                if(valueMap[r][c] === v1) {
+                    let groupNum = groupNumMap[r][c];
+                    group.set(groupNum, v2);
+                    valueMap[r][c] = v2;
+                } 
+            }    
+        }
+    }
+    
+    function merge(r1, c1, r2, c2, gn){
+        let [v1, v2] = [valueMap[r1][c1], valueMap[r2][c2]];
+        let [g1, g2] = [groupNumMap[r1][c1], groupNumMap[r2][c2]];
+        
+        let value = v1;
+        
+        if(v1 === 'EMPTY' && v2 !== 'EMPTY') {
+            value = v2;
+        }
+        if (g1 !== 0 && g2 !== 0 && g1 === g2) {
+            return false;
+        }
+        
+        [valueMap[r1][c1], groupNumMap[r1][c1]] = [value, gn];
+        [valueMap[r2][c2], groupNumMap[r2][c2]] = [value, gn];
+       
+
+        for(let r = 1; r < 51; r++){
+            for(let c = 1; c < 51; c++){
+                if((g1 !== 0 && groupNumMap[r][c] === g1) || (g2 !== 0 && groupNumMap[r][c] === g2)) {
+                    groupNumMap[r][c] = gn;
+                    valueMap[r][c] = value;
+                } 
+            }    
+        }
+        group.set(gn, value);
+        
+        
+        return true;
+    }
+    
+    function unmerge(r, c){
+        let value = valueMap[r][c];
+        let groupNum = groupNumMap[r][c];
+        
+        if(groupNum === 0) return;
+        
+        for(let ri = 1; ri < 51; ri++){
+            for(let cj = 1; cj < 51; cj++){
+                if(groupNumMap[ri][cj] === groupNum) {
+                    [groupNumMap[ri][cj],valueMap[ri][cj]] = [0,'EMPTY'];
+                } 
+            }    
+        }
+        valueMap[r][c] = value;
+        groupNumMap[r][c] = groupNum;
+    }
+    
+    function print(r, c){
+        answer.push(valueMap[r][c]);
+    }   
+   
     return answer;
-}
-
-function updateOne(rest, map){
-    const [r, c, v] = rest;
-    const group = map[r][c][0];
-    map[r][c][1] = v;
-    
-    if(group !== 0){
-        for(let i = 1; i < 51; i++){
-            for(let j = 1; j < 51; j++){
-                if(map[i][j][0] === group) map[i][j][1] = v;
-            }
-        }
-    }
-}
-function updateAll(rest, map){
-    const [v1, v2] = rest;
-    
-    for(let i = 1; i < 51; i++){
-        for(let j = 1; j < 51; j++){
-            if(map[i][j][1] === v1) map[i][j][1] = v2;
-        }
-    }
-}
-
-function merge(rest, map, groupNum){
-    const [r1, c1, r2, c2] = rest;
-    const [gn1, v1] = map[r1][c1];
-    const [gn2, v2] = map[r2][c2];
-    
-    let value = 'EMPTY';
-    
-    if (gn1 !== 0 && gn2 !== 0 && gn1 === gn2) {
-        return false;
-    }
-    
-    if(v1 === 'EMPTY' && v2 !== 'EMPTY') value = v2;
-    else value = v1;
-    
-    map[r1][c1] = [groupNum, value];
-    map[r2][c2] = [groupNum, value];
-    
-    for(let i = 1; i < 51; i++){
-        for(let j = 1; j < 51; j++){
-            if((gn1 !== 0 && map[i][j][0] === gn1) || (gn2 !== 0 && map[i][j][0] === gn2)) {
-                map[i][j] = [groupNum,value];
-            }            
-        }
-    }
-    return true;
-}
-function unmerge(rest, map){
-    const [r,c] = rest;
-    const [group, value] = map[r][c];
-    
-    if(group === 0) return;
-    
-    for(let i = 1; i < 51; i++){
-        for(let j = 1; j < 51; j++){
-            if(map[i][j][0] === group) map[i][j]= [0, 'EMPTY'];
-        }
-    }
-    map[r][c] = [group, value];
 }
