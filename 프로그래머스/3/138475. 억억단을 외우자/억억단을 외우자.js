ce8afa9af76a031303ba2b00b62ca9cb2df29d9a
pthwan27 <65005680+pthwan27@users.noreply.github.com>
2024-05-24 01:39:08 +0900

    [level 3] Title: 억억단을 외우자, Time: 744.56 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/3/138475. 억억단을 외우자/억억단을 외우자.js
+++ 프로그래머스/3/138475. 억억단을 외우자/억억단을 외우자.js
@@ -1,20 +1,22 @@
 function solution(e, starts) {
-    let count = new Array(e+1).fill(0);
+    let cntArr = new Array(e+1).fill(0);
     
     for(let i = 1; i <= e; i++){
-        for(let j = i; j <= e; j += i){
-            count[j]++;
+        for(let j = i; j <= e; j+=i){
+            cntArr[j]++;
         }
     }
-    let maxArr = new Array(e+1).fill(e);
-    let max = count[e];
-    for(let i = e, mIdx = e; i > 0; i--){
-        if(count[i] >= max) {
-            mIdx = i;
-            max = count[i];
-        }        
-        maxArr[i] = mIdx;
-    }
     
-    return starts.map(e => maxArr[e]);
+    let maxCntArr = new Array(e+1).fill(e);
+    let max = cntArr[e];
+    
+    for(let i = e - 1; i >= 1; i--){
+        if(max <= cntArr[i]) {
+            maxCntArr[i] = i;
+            max = cntArr[i];
+        }
+        else maxCntArr[i] = maxCntArr[i+1];
+    }
+
+    return starts.map(e => maxCntArr[e]);
 }
