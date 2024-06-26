915e7e8248b2bba42d0581cc1c7cd3876b41f8f0
pthwan27 <65005680+pthwan27@users.noreply.github.com>
2024-06-26 19:40:10 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 26.45 ms, Memory: 59.5 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/README.md
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/README.md
@@ -1,10 +1,10 @@
 # [level 3] 연속 펄스 부분 수열의 합 - 161988 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161988?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161988) 
 
 ### 성능 요약
 
-메모리: 58.9 MB, 시간: 23.88 ms
+메모리: 59.5 MB, 시간: 26.45 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 05일 17:25:05
+2024년 06월 26일 19:40:06
 
 ### 문제 설명
 
--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.js
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.js
@@ -1,24 +1,25 @@
 function solution(sequence) {
     let answer = 0;
     
-    let minusDP = new Array(sequence.length).fill(0);
-    let plusDP = new Array(sequence.length).fill(0);
+    let dpPM = new Array(sequence.length).fill(-Infinity);
+    let dpMP = new Array(sequence.length).fill(-Infinity);
     
-    plusDP[0] = +sequence[0]; 
-    minusDP[0] = -sequence[0];
+    let PM = +1;
+    let MP = -1;
     
-    let plus = 1;
-    let minus = -1;
+    dpPM[0] = sequence[0] * PM;
+    dpMP[0] = sequence[0] * MP;
     
     for(let i = 0; i < sequence.length; i++){
-        if(i > 0){
-            plusDP[i] = Math.max(sequence[i] * plus, plusDP[i - 1] + sequence[i] * plus);
-            minusDP[i] = Math.max(sequence[i] * minus, minusDP[i - 1] + sequence[i] * minus);
+        if(i > 0) {
+            dpPM[i] = Math.max(dpPM[i-1] + sequence[i] * PM, sequence[i] * PM);
+            dpMP[i] = Math.max(dpMP[i-1] + sequence[i] * MP, sequence[i] * MP);
         }        
-        plus *= -1;
-        minus *= -1;
         
-        answer = Math.max(answer, plusDP[i], minusDP[i]);
+        PM *= -1;
+        MP *= -1;
+        
+        answer = Math.max(answer, Math.max(dpPM[i], dpMP[i]));
     }
     
     return answer;
