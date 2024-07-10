7faf73abc2e529fca4d2dd9c1c26227ac3db5db0
pthwan27 <65005680+pthwan27@users.noreply.github.com>
2024-07-10 17:23:22 +0900

    [Gold V] Title: 리모컨, Time: 728 ms, Memory: 46440 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.js
+++ 백준/Gold/1107. 리모컨/리모컨.js
@@ -15,66 +15,35 @@ rl.on("close", () => {
   let M = Number(input[1]);
 
   const buttons = new Array(10).fill(true);
-  let plus = true;
-  let minus = true;
 
   if (N === 100) {
     console.log(0);
     return;
   }
 
-  if (input[2]) {
+  if (M) {
     input[2].split(" ").map((e) => {
-      if (e === "+") plus = false;
-      else if (e === "-") minus = false;
-      else buttons[e] = false;
+      buttons[e] = false;
     });
   }
 
   let targetCh = N;
   let targetChArr = targetCh.toString().split("").map(Number);
 
-  let chPlus = 100;
-  let chPlusCnt = Math.min(Math.abs(targetCh - 100), targetChArr.length + Math.abs(targetCh - 100));
-  let minPlusCnt = chPlusCnt;
+  let cnt = Math.min(Math.abs(targetCh - 100), targetChArr.length + Math.abs(targetCh - 100));
+  let minCnt = cnt;
 
-  let chMinus = 100;
-  let chMinusCnt = Math.min(
-    Math.abs(targetCh - 100),
-    targetChArr.length + Math.abs(targetCh - 100)
-  );
-  let minMinusCnt = chMinusCnt;
-
-  while (plus && chPlus < targetCh * 2 - 100) {
-    chPlus++;
-    chPlusCnt++;
-
-    let ch = chPlus.toString().split("").map(Number);
-
-    if (chPlus === targetCh) minPlusCnt = Math.min(chPlusCnt, minPlusCnt);
+  for (let i = 0; i < 1000000; i++) {
+    let ch = i.toString().split("").map(Number);
 
     if (isAble(ch, buttons)) {
-      chPlusCnt = Math.min(ch.length + Math.abs(targetCh - chPlus), chPlusCnt);
-      minPlusCnt = Math.min(chPlusCnt, minPlusCnt);
+      minCnt = Math.min(ch.length + Math.abs(targetCh - i), minCnt);
     }
   }
 
-  while (minus && chMinus > 0) {
-    chMinus--;
-    chMinusCnt++;
-
-    let ch = chMinus.toString().split("").map(Number);
-
-    if (chMinus === targetCh) minMinusCnt = Math.min(chMinusCnt, minMinusCnt);
-
-    if (isAble(ch, buttons)) {
-      chMinusCnt = Math.min(ch.length + Math.abs(targetCh - chMinus), chMinusCnt);
-      minMinusCnt = Math.min(chMinusCnt, minMinusCnt);
-    }
-  }
-
-  console.log(Math.min(minPlusCnt, minMinusCnt));
+  console.log(minCnt);
 });
+
 function isAble(ch, buttons) {
   for (let i = 0; i < ch.length; i++) {
     if (!buttons[ch[i]]) {
