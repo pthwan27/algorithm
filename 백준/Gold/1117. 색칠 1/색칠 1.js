823d32c4bf17ee81a175171eae22dcbdc2d69e5d
pthwan27 <65005680+pthwan27@users.noreply.github.com>
2024-07-22 16:27:29 +0900

    [Gold V] Title: 색칠 1, Time: 168 ms, Memory: 9488 KB -BaekjoonHub

---
+++ 백준/Gold/1117. 색칠 1/색칠 1.js
@@ -0,0 +1,34 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = [];
+rl.on("line", (e) => {
+  input.push(e);
+});
+
+rl.on("close", () => {
+  let [W, H, f, c, x1, y1, x2, y2] = input[0].split(" ").map(BigInt);
+
+  let paintW = BigInt(x2 - x1);
+  let paintH = BigInt(y2 - y1);
+
+  let answer = W * H;
+
+  let dbWidth = f > W / 2n ? W - f : f;
+
+  if (dbWidth <= x1) {
+    answer -= paintW * paintH * (c + 1n);
+  } else if (dbWidth < x2) {
+    answer -= paintH * (dbWidth - x1) * (c + 1n) * 2n;
+
+    answer -= paintH * (x2 - dbWidth) * (c + 1n);
+  } else {
+    answer -= paintH * paintW * (c + 1n) * 2n;
+  }
+
+  console.log(answer.toString());
+});
